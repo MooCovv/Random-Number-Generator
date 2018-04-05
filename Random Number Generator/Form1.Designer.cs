9f46c06a32bcc48b8c7ccff78895388a040b8aa3
PHILIP ROBINSON <philip.robinson@lausd.net>
2018-04-04 19:09:17 -0700

    We did it!

--- Random Number Generator/Form1.Designer.cs
+++ Random Number Generator/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numberListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.populateButton = new System.Windows.Forms.Button();
+            this.valueBox1 = new System.Windows.Forms.TextBox();
+            this.valueBox2 = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberListBox
+            // 
+            this.numberListBox.FormattingEnabled = true;
+            this.numberListBox.Location = new System.Drawing.Point(12, 25);
+            this.numberListBox.Name = "numberListBox";
+            this.numberListBox.Size = new System.Drawing.Size(169, 264);
+            this.numberListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Value 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Value 2";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(208, 206);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // populateButton
+            // 
+            this.populateButton.Location = new System.Drawing.Point(208, 150);
+            this.populateButton.Name = "populateButton";
+            this.populateButton.Size = new System.Drawing.Size(75, 23);
+            this.populateButton.TabIndex = 4;
+            this.populateButton.Text = "Populate";
+            this.populateButton.UseVisualStyleBackColor = true;
+            this.populateButton.Click += new System.EventHandler(this.populateButton_Click);
+            // 
+            // valueBox1
+            // 
+            this.valueBox1.Location = new System.Drawing.Point(249, 40);
+            this.valueBox1.Name = "valueBox1";
+            this.valueBox1.Size = new System.Drawing.Size(45, 20);
+            this.valueBox1.TabIndex = 5;
+            // 
+            // valueBox2
+            // 
+            this.valueBox2.Location = new System.Drawing.Point(249, 96);
+            this.valueBox2.Name = "valueBox2";
+            this.valueBox2.Size = new System.Drawing.Size(45, 20);
+            this.valueBox2.TabIndex = 6;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(208, 261);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(310, 303);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.valueBox2);
+            this.Controls.Add(this.valueBox1);
+            this.Controls.Add(this.populateButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberListBox);
+            this.Name = "Form1";
+            this.Text = "Random Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox numberListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button populateButton;
+        private System.Windows.Forms.TextBox valueBox1;
+        private System.Windows.Forms.TextBox valueBox2;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- Random Number Generator/Form1.cs
+++ Random Number Generator/Form1.cs
@@ -16,5 +16,42 @@ namespace Random_Number_Generator
         {
             InitializeComponent();
         }
+
+        private void populateButton_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            for (int i = 1; i < 21; i++)
+            {
+                int number;
+                number = rand.Next(99) + 1;
+                numberListBox.Items.Add(number);
+
+                if (i == 5)
+                {
+                    valueBox1.Text = number.ToString();
+                }
+
+                if (i == 15)
+                {
+                    valueBox2.Text = number.ToString();
+                }
+            }
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int value1 = int.Parse(valueBox1.Text);
+            int value2 = int.Parse(valueBox2.Text);
+
+            int outPut = value1 + value2;
+
+            MessageBox.Show(outPut.ToString());
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
